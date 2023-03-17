0c4e43b8932bb01f4eeaa2f082d7b3107d932916
Gaby Nieva <gaby.nieva@outlook.com>
2023-03-17 14:32:15 -0600

    Strict

---
+++ src/scope/strict.js
@@ -0,0 +1,10 @@
+'use strict'; // debes declarar y asignar
+pi = 3.1416; // se asigna el valor sin declarar la variable
+console.log(pi);
+
+function myfunction(){
+    'use strict';
+    return pi = 3.1416;
+}
+
+console.log(myfunction);
