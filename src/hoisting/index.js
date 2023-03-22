dbbf80c14372492895a1d43aaeeb4be932723c8c
Gaby Nieva <gaby.nieva@outlook.com>
2023-03-22 15:59:51 -0600

    Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,11 @@
+// console.log(nameOfDog);
+// var nameOfDog = 'Gings';
+// console.log(nameOfDog);
+
+nameOfDog();
+
+function nameOfDog(){
+    console.log(`El perrito más hermoso es ${gings}`);
+}
+
+var gings = 'Gingerita';
